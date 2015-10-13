69065f9201b82c808152dc0d3207092e64c616f8
Fylwind <fyl@wolfpa.ws>
2015-10-13 04:04:42 -0400

    More messing around

--- sh-ulc.hs
+++ sh-ulc.hs
@@ -1,16 +1,27 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds, GADTs, KindSignatures #-}
 module Main where
 import Data.Foldable
 import System.IO
 
-data Literal where
-  Str :: String -> Literal
-  deriving (Eq, Read, Ord, Show)
-
 data Expr where
   App :: Expr -> Expr -> Expr
   Lam :: (Expr -> Expr) -> Expr
-  Lit :: Literal -> Expr
+  Lit :: String -> Expr
+  Var :: String {- varname -} -> Expr
+  Bnd :: Int -> Expr
+
+-- newtype Shell (t :: ShellType) = Shell String
+
+-- data ShellType = UString | UExit | UStream
+
+-- byVar :: String -> Shell 'UString
+-- byVar s = Shell _
+
+-- byExit :: String -> Shell 'UExit
+-- byExit s = Shell _
+
+-- byOut :: String -> Shell 'UStream
+-- byOut s = Shell _
 
 shellEscape :: String -> String
 shellEscape s = "'" ++ s ++ "'" -- FIXME
@@ -41,32 +52,44 @@ shRuntime runtime =
   [ ""
   ]
 
-toShell :: Expr -> String
-toShell (Lit (Str s)) = primRet ++ "=" ++ shellEscape s
-toShell (App f x) =
+deBruijnize :: Int -> Expr -> String
+deBruijnize i (App f x) = App (deBruijnize i f) (deBruijnize i x)
+deBruijnize i (Bnd j) = Bnd (i - j)
+deBruijnize i (Lam f) = Lam' (deBruijnize i' (f (Bnd i')))
+  where i' = i + 1
+deBruijnize _ e       = e
+
+toShell :: Int -> Expr -> String
+toShell _ (Lit s) = primRet ++ "=" ++ shellEscape s
+toShell i (App f x) =
   unlines
   [ f'
   , primPush ++ " \"$" ++ primRet ++ "\""
   , x'
   , primApply
   ]
-  where f' = toShell f
-        x' = toShell x
-toShell (Lam f) =
+  where f' = toShell i f
+        x' = toShell i x
+toShell i (Lam f) =
   unlines
   [ undefined
   ]
+  where _ = toShell i' (f (Bnd i'))
+        i' = i + 1
+toShell i (Bnd j) =
+  undefined
+  where v = i - j -- de Bruijn index
 
-litFun name = Lit (Str (name ++ " \"$1\""))
+litFun name = Lit (name ++ " \"$1\"")
 
 main :: IO ()
 main = do
   hSetNewlineMode stdout noNewlineTranslation
   shRuntime <- readFile "sh-rt.sh"
   putStr shRuntime
-  putStr . toShell $
+  putStr . toShell 0 $
     App (litFun primPrint)
     (App (App (litFun primConcat)
-          (Lit (Str "hello "))) (Lit (Str "world")))
+          (Lit "hello ")) (Lit "world"))
 
 -- time bash -c '. ./sh-rt.sh; for i in {1..10000}; do : _ts_rt_expr x 1 + 1; done'
